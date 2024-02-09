d8be1114012dabf44fa7b8ef59f828dc46e6f138
Marc Wagner <marcch1993@gmail.com>
2024-02-09 17:56:17 +0100

    init project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Interactive Dashboard for Ultimate Frisbee strategy analysis',
+    author='Marc-Christoph Wagner',
+    license='MIT',
+)
