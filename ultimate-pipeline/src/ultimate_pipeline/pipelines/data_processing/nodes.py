69877880b00bde19c0c177e64372686ce583bcd3
adamjasinski <adam.jasinski@yahoo.ie>
2024-03-10 17:37:49 +0100

    Cosmetic changes in annotations

--- ultimate-pipeline/src/ultimate_pipeline/pipelines/data_processing/nodes.py
+++ ultimate-pipeline/src/ultimate_pipeline/pipelines/data_processing/nodes.py
@@ -2,8 +2,9 @@
 This is a boilerplate pipeline 'data_processing'
 generated using Kedro 0.19.3
 """
-
-import typing as t
+import os
+import sys
+from typing import Callable, Any
 import pandas as pd
 
 import logging
@@ -13,14 +14,14 @@ from .supervisely_downloader import helper_download_image_dataset_from_supervise
 
 logger = logging.getLogger(__name__)
 
-def download_image_dataset_from_supervisely(params: t.Dict) -> t.Dict[str, t.Any]:
+def download_image_dataset_from_supervisely(params: dict[str, Any]) -> dict[str, Any]:
     
     logger.info("Downloading dataset from Supervisely")
 
     return helper_download_image_dataset_from_supervisely(params)
 
 
-def create_yolo_dataframe_partitions(df: pd.DataFrame) -> t.Dict[str, pd.DataFrame]:
+def create_yolo_dataframe_partitions(df: pd.DataFrame) -> dict[str, pd.DataFrame]:
     """
     Split the DataFrame by 'frame' column into a dictionary of partitioned data frames 
     keyed by 'frame' (which corresponds to a video frame number or name). 
@@ -29,7 +30,7 @@ def create_yolo_dataframe_partitions(df: pd.DataFrame) -> t.Dict[str, pd.DataFra
     return { str(frame): data for frame, data in grouped }
 
 def convert_supervisely_images_annotations_to_dataframe(
-        annotation_partitions: dict[str, t.Callable[[],dict]], 
+        annotation_partitions: dict[str, Callable[[], dict[str, Any]]], 
         meta_file: dict) -> pd.DataFrame:
     """
     Convert Supervisely image annotations from a given folder to a DataFrame containing bounding boxes.
@@ -45,7 +46,7 @@ def convert_supervisely_images_annotations_to_dataframe(
 
     return convert_images_annotations_folder(source=annotation_partitions_without_extension, meta_file=meta_file)
 
-def copy_dataset_items(items: list):
+def copy_dataset_items(items: dict[str, Any]) -> dict[str, Any]:
     """Copy data between datasets"""
     return items
 
