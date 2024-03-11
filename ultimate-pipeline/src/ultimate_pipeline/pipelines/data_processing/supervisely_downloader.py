5823b8d79575241e6c94d1f64c947a77606c6334
Marc Wagner <marcch1993@gmail.com>
2024-03-11 14:35:07 +0100

    added lambda like expressions to helper_download

--- ultimate-pipeline/src/ultimate_pipeline/pipelines/data_processing/supervisely_downloader.py
+++ ultimate-pipeline/src/ultimate_pipeline/pipelines/data_processing/supervisely_downloader.py
@@ -8,6 +8,7 @@ import logging
 from dotenv import load_dotenv
 from PIL import Image
 from pathlib import Path
+from functools import partial
 
 logger = logging.getLogger(__name__)
 
@@ -25,7 +26,17 @@ def helper_download_image_dataset_from_supervisely(params: t.Dict) -> t.Tuple[t.
             - annotations_dict: A dictionary containing the annotations for each image
             - images_dict: A dictionary containing the images
     """
+
+    def download_image(img: sly.api.image_api.ImageInfo, api:sly.api) -> np.ndarray:
+        """ Download image from Supervisely """
+        logger.info(f"Start downloading image {img.name} from Supervisely")
+        return Image.fromarray(api.image.download_np(img.id))
     
+    def download_annotation(img: sly.api.image_api.ImageInfo, api:sly.api)-> t.Dict[str, t.Any]:
+        """ Download annotation from Supervisely """
+        logger.info(f"Start downloading annotation for image {img.name} from Supervisely")
+        return api.annotation.download(img.id).annotation
+
     if sly.is_development():
         load_dotenv(os.path.expanduser("~/supervisely.env"))
 
@@ -39,42 +50,8 @@ def helper_download_image_dataset_from_supervisely(params: t.Dict) -> t.Tuple[t.
     images_dict = {}
 
     for image in image_info_list:
-
         image_name = Path(image.name).stem
-
-        images_dict[image_name] = download_image_lazy(image, api)
-        annotations_dict[image.name] = download_annotation_lazy(image, api)
+        images_dict[image_name] = partial(download_image, image, api)
+        annotations_dict[image.name] = partial(download_annotation, image, api)
 
     return metadata, annotations_dict, images_dict
-
-
-def download_image_lazy(image: sly.api.image_api.ImageInfo, api: sly.api):
-    """ Download image from Supervisely lazily
-    
-        Args: 
-            image: The image to download
-            api: The Supervisely API
-        Returns:
-            A function that downloads the image
-    """
-    def download_image() -> np.ndarray:
-        logger.info(f"Start downloading image {image.name} from Supervisely")
-        return Image.fromarray(api.image.download_np(image.id))
-    
-    return download_image
-
-
-def download_annotation_lazy(image: sly.api.image_api.ImageInfo, api: sly.api) -> t.Callable[[], np.ndarray]:
-    """ Download annotation from Supervisely lazily
-    
-        Args: 
-            image: image for which the annotation should be downloaded
-            api: The Supervisely API
-        Returns:
-            A function that downloads the annotation
-    """
-    def download_annotation()-> t.Dict[str, t.Any]:
-        logger.info(f"Start downloading annotation for image {image.name} from Supervisely")
-        return api.annotation.download(image.id).annotation
-    
-    return download_annotation
