352adc0e0ff649c68ff152cff6ee5af22fdbb572
adamjasinski <adam.jasinski@yahoo.ie>
2024-03-28 23:26:41 +0100

    Cosmetic changes

--- web-app/tests/integration/test_keypoints_integration.py
+++ web-app/tests/integration/test_keypoints_integration.py
@@ -16,28 +16,28 @@ def _read_prediction_dataframe_from_dir(folder: str, is_tracking=True) -> pd.Dat
 
     return pd.concat(dfs)
 
+
 # In order to run the test, make sure that it points to a folder with YOLO8 object tracking results (txt files) and remove the skip decorator.
 @pytest.mark.skip("Integration tests, not part of unit tests")
 @pytest.mark.parametrize("folder_path", [("../ultimate-pipeline/runs/detect/track6/labels")])
 def test_get_4_best_keypoint_pairs_on_real_folder(folder_path):
     df = _read_prediction_dataframe_from_dir(folder_path)
-    assert len(df) > 64000
+    expected_detections = 64057
+    expected_video_frames = 2935
 
-    bad_ones = []
+    assert len(df) == expected_detections
+
+    unprocessable_frames = []
     sut = KeypointsExtractor(df, conf_threshold=0.5, max_lookback=30)
-    got_value_error = False
-    for i in range(1, 2935):
+    for i in range(1, expected_video_frames):
         try:
             result = sut.get_4_best_keypoint_pairs(frame_no=i)
         except ValueError:
-            bad_ones.append(i)
-            print(f"hj d gfValueError for: {i}")
-            got_value_error = True
-        if got_value_error:
-            got_value_error = False
-            continue
+            unprocessable_frames.append(i)
+
         assert result is not None, f"result is None for frame {i}"
+
         H = calculate_homography_matrix(result)
         assert H is not None, f"Homography matrix is None for frame {i}"
         assert H.shape == (3,3)
-    assert len(bad_ones) == 0
+    assert len(unprocessable_frames) == 0
