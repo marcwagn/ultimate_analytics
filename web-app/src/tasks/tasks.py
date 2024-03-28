04ef84053f0aabfc55b266fd1057d82864dd29a6
adamjasinski <adam.jasinski@yahoo.ie>
2024-03-28 15:50:10 +0100

    tasks + yolo_helper

--- web-app/src/tasks/tasks.py
+++ web-app/src/tasks/tasks.py
@@ -1,26 +1,73 @@
-import time
-import cv2
-
 from celery import shared_task
 from celery import Task
 from celery.utils.log import get_task_logger
+from yolo_helper import make_callback_adapter_with_counter, convert_tracking_results_to_pandas
+from src.keypoints import KeypointsExtractor, KeypointQuad
+from src.homography import calculate_homography_matrix, convert_h
+from src.video_object import VideoObject
+from src.team_detector import TeamDetector
 
 logger = get_task_logger(__name__)
 
 
 @shared_task(bind=True, ignore_result=False)
 def video_analysis(self: Task, video_path: str) -> object:
-    logger.info(f"Start analyis for video: {video_path}")
+    logger.info(f"Start analysis for video: {video_path}")
     self.update_state(state="PROGRESS", meta={"status": 0})
 
-    video = cv2.VideoCapture(video_path)
-    frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    video_object = VideoObject(video_path=video_path)
 
-    for frame in range(0,frames,100):
-        self.update_state(state="PROGRESS", meta={"status": frame / frames })
-        
-        
-        #logger.info(f"Detected frames: {frames}")
-        time.sleep(1)
+    #video = cv2.VideoCapture(video_path)
+    #total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    total_frames = video_object.get_num_frames()
 
-    return {"status": 100}
+    def update_progressbar(frame):
+        if frame % 100:
+            logger.info(f"YOLO object tracking for {video_path}: frame {frame}")
+        self.update_state(state="PROGRESS", meta={"status": frame / total_frames })
+
+    model_path = "TODO"
+    tracking_results = track(self, model_path=model_path, video_path=video_path, progressbar_callback=update_progressbar)
+
+    # Team detection
+    for frame in range(0, total_frames):
+        img = video_object.get_frame(frame)
+        team_detector = TeamDetector(img=img, detector_results=tracking_results)
+        team_prediction = team_detector.predict_player_clusters(player_imgs=TODO)
+
+    # Keypoints and perspective removal
+    tracking_results_df = convert_tracking_results_to_pandas(tracking_results)
+
+    keypoints_extractor = KeypointsExtractor(tracking_results_df, conf_threshold=0.5, max_lookback=30)
+    H_all = [calculate_homography_matrix(keypoints_extractor.get_4_best_keypoint_pairs(frame)) for frame in range(0, total_frames)]
+    # TODO - make it in 1 pass
+    tracking_results_df["x_t"] = tracking_results_df.apply(lambda row: convert_h(H_all[row["frame"]], row[1:3].to_numpy())[0], axis=1)
+    tracking_results_df["y_t"] = tracking_results_df.apply(lambda row: convert_h(H_all[row["frame"]], row[1:3].to_numpy())[1], axis=1)
+
+
+    # for frame in range(0,total_frames,100):
+    #     #logger.info(f"Detected frames: {frames}")
+    #     time.sleep(1)
+
+     # TODO
+    tracking_results_dict = tracking_results_df
+    return {"status": tracking_results_dict }
+
+def track(self, model_path: str, video_path: str, progressbar_callback: Callable) -> Any
+        """
+        Perform object tracking on the video with YOLOv8.
+        Args:
+            progressbar_callback (Callable[int]): a callback accepting 1 argument (frame number)
+        Return:
+            YOLO tracking results
+        """
+        model = ultralytics.YOLO(model_path, verbose=True)
+        yolo_progress_reporting_event = "on_predict_batch_start"
+        progress_callback_wrapped = make_callback_adapter_with_counter(yolo_progress_reporting_event, 
+                                                                       lambda _,counter: progressbar_callback(counter))
+        model.add_callback(yolo_progress_reporting_event, progress_callback_wrapped)
+
+        device = 0 if torch.cuda.is_available() else 'cpu' 
+        tracking_results = model.track(source=video_path, agnostic_nms=True, show=False, device=device, stream=True)
+
+        return tracking_results
---
+++ web-app/src/tasks/yolo_helper.py
@@ -0,0 +1,64 @@
+import ultralytics
+import pandas as pd
+import numpy as np
+
+frames = 0
+
+def make_callback_adapter_with_counter(event_name, callback):
+    """
+    Convert the callback function with 2 params to a callback format required by YOLO.
+    Args:
+        event_name: str: YOLO pipeline event name
+        callback: Callable(str, int): a callback function accepting an 2 params: event_name and counter
+    Return:
+        A callback in the format required by YOLO.
+    """
+    event_counter = 0
+
+    def yolo_callback(component):
+        nonlocal event_counter
+        event_counter += 1
+        callback(event_name, event_counter)
+
+    return yolo_callback
+
+def _convert_single_tracking_result(frame_no, boxes_result:ultralytics.engine.results.Boxes):
+    box = boxes_result.boxes # sic!
+    int_vectorized = np.vectorize(np.int_, otypes=[int])
+    if box is not None:
+        class_ids = int_vectorized(box.cls.cpu().numpy())
+        observation_count = len(class_ids)
+        class_id_to_name = lambda id: boxes_result.names[int(id)]
+        class_names = list(map(class_id_to_name, class_ids))
+        ids = int_vectorized(box.id.cpu()) if box.id is not None else np.zeros(shape=observation_count, dtype='int')
+        xywh = box.xywh.cpu()
+        xs = xywh[:, 0]
+        ys = xywh[:, 1]
+        ws = xywh[:, 2]
+        hs = xywh[:, 3]
+        frame_nos = np.repeat(a=frame_no, repeats=observation_count)
+        data = dict(frame_no=frame_nos, class_id=class_ids, class_name=class_names, id=ids, x=xs, y=ys, w=ws, h=hs)
+        df = pd.DataFrame(data=data)
+        return df
+    else:
+        return pd.DataFrame(columns=['frame_no','class_id', 'class_name', 'id', 'x', 'y', 'w', 'h'])
+
+def convert_tracking_results_to_pandas(tracking_results):
+    """
+    Convert YOLOv8 tracking output to a Pandas DataFrame.
+    The DataFrame contains the following columns:
+        - frame_no:int - frame number
+        - class_id:int - class identifier
+        - class_name:str - class name of the tracked object
+        - id:int - identifier of the tracked object
+        - x:int - coordinates of the bounding boxes
+        - y:int
+        - w:int
+        - h::int
+    """
+    dfs = [] # Will contain 1 data frame per video frame
+    for i, tr in enumerate(tracking_results):
+        df = _convert_single_tracking_result(i, tr)
+        dfs.append(df)
+
+    return pd.concat(dfs)
