5948317ed5e38f4e7dd4e88a3306a0f178828d2b
adamjasinski <adam.jasinski@yahoo.ie>
2024-04-01 22:03:56 +0200

    integration test case

--- web-app/tests/integration/test_video_analysis.py
+++ web-app/tests/integration/test_video_analysis.py
@@ -23,16 +23,24 @@ def test_video_analysis_can_run_prediction_on_video(video_path):
     assert set(results["1"][0].keys()).issuperset(set(expected_columns))
 
 
-@pytest.mark.parametrize("pickled_results_path", [("./tests/data/tracking_set_ultralytics/tracking_results.pickle")])
-def test_video_analysis_final_translation(pickled_results_path):
+@pytest.mark.parametrize("pickled_results_path,expected_total_frames", [
+                             ("./tests/data/tracking_set_ultralytics/tiny_tracking_results.pickle", 10),
+                             ("./tests/data/tracking_set_ultralytics/machine_vs_condors_pool_001.pickle", 1501)])
+def test_video_analysis_final_translation_case_pool_001a(pickled_results_path,expected_total_frames):
     with open(pickled_results_path, "rb") as f:
         tracking_results = pickle.load(f)
 
-    expected_total_frames = 10
+    expected_total_frames = expected_total_frames
     df = tasks._translate_coordinates(tracking_results, expected_total_frames)
 
     assert df is not None
     assert len(df["frame"].unique()) == expected_total_frames
     expected_columns = ["cls", "x", "y", "id", "frame"]
     assert set(df.columns).issuperset(set(expected_columns))
+    df_nas = df[df["x"].isna()]
+    na_count = len(df_nas)
+    print(f"Found {na_count} NAs")
+    print(df_nas.head())
+    assert na_count == 0
     assert not df.isna().any(axis=None)
+    
