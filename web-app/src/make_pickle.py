e95e012c050c9856c39fede1aba158ef20444659
adamjasinski <adam.jasinski@yahoo.ie>
2024-04-02 14:59:03 +0200

    make_pickle cleanup

--- web-app/src/make_pickle.py
+++ web-app/src/make_pickle.py
@@ -5,26 +5,21 @@ from tasks.tasks import video_analysis
 from pathlib import Path
 import os
 
-# def _calculate_sha256sum(filename):
-#     with open(filename, 'rb', buffering=0) as f:
-#         return hashlib.file_digest(f, 'sha256').hexdigest()
-    
-# Command line tool to run Ultralytics YOLO object tracking and pickle the results
-
-# def _track():
-#     model = ultralytics.YOLO(model_path, verbose=True)
-#     yolo_progress_reporting_event = "on_predict_batch_start"
-#     progress_callback_wrapped = make_callback_adapter_with_counter(yolo_progress_reporting_event, 
-#                                                                     lambda _,counter: progressbar_callback(counter))
-#     model.add_callback(yolo_progress_reporting_event, progress_callback_wrapped)
-
-#     # NB - if torch package is installed in the CPU variant, the device will default to "cpu"
-#     device = 0 if torch.cuda.is_available() else "cpu" 
-#     tracking_results = model.track(source=video_path, agnostic_nms=True, show=False, device=device, stream=True)
-
-
 def main():
+    """
+    Command line tool to run Ultimate board detection and pickle the results.
+    The result data is a Pythondictionary in the following format:
+        key (str): frame number
+        value (object):
+            cls (int): class id of the detected object (0: player; 29: frisbee disc; 30: referee)
+            x (float): mid-point x coordinate of the detected object, expressed in 'real ultimate pitch' units
+            y (float): mid-point y coordinate of the detected object, expressed in 'real ultimate pitch' units
+            team (int): team number (typically 0 or 1)
+            id (int): YOLO instance id of the detected object; can be used to keep track of objects across multiple frames
+    """
     if len(sys.argv) < 2:
+        print(f"{sys.argv[0]} is a tool to run Ultimate board detection and pickle the results for a given Ultimate Frisbee video.")
+        print("The results are saved to a file <video_sha256>.pickle in [output_dir].")
         print(f"Usage: {sys.argv[0]} path_to_video [output_dir]")
         sys.exit(-1)
 
@@ -37,31 +32,13 @@ def main():
 
     video_sha_sum = _calculate_sha256sum(video_path)
     print(f"SHA-256 checksum of {video_path} is {video_sha_sum}")
-    #model_path = "./data/model/best.pt"
 
     analysis_results = video_analysis(video_path)
     if analysis_results is not None and "coordinates" in analysis_results:
         streamlined_results = analysis_results["coordinates"]
     else:
-        raise RuntimeError("Unexpected results returned from video_analysis")
-    # tracking_results = _track(model_path=model_path, video_path=video_path, progressbar_callback=None)
-
-    # print("Streamlining the results...")
-    # streamlined_results = []
-
-    # for i, result in enumerate(tracking_results):
-    #     encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 80]
-    #     jpg_result, encimg = cv2.imencode('.jpg', result.orig_img, encode_param)
-    #     if not jpg_result:
-    #          raise RuntimeError(f"JPEG compression failed for frame {i}")
-    #     result.orig_img = encimg
-    #     streamlined_results.append(result)
-
-    # Removing orig_img
-    # for result in tracking_results:
-    #     del result.orig_img
-    #     streamlined_results.append(result)
-
+        raise RuntimeError("Unexpected results returned from Ultimate Board video_analysis")
+    
     output_file = Path(output_dir)/f"{video_sha_sum}.pickle"
     with open(output_file, "wb") as f:
             pickle.dump(streamlined_results, f)
